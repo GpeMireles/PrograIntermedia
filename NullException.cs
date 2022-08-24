f656c589392bd0e00f8c9734d58e59dcf7ab7ded
Jose Guadalupe Mireles <guadalupe__mireles@hotmail.com>
2022-08-24 16:30:30 -0500

    Hasta punto 3

---
+++ NullException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace PrograIntermedia {
+    internal class NullException : NullReferenceException {
+        public NullException(string message) : base(message) { }
+
+        public bool ThrowNullException() {
+            throw this;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,36 @@ namespace PrograIntermedia {
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            
+            // 1.- Operador ternario
+            Random random = new Random();
+            int variableRandom = random.Next(0,1000);
+            string texto = variableRandom > 500 ? "Grater than 500" : "Less than";
+
+            // 2.- Nullable & Exceptions Creation
+            double? nullable = null;
+            try {
+                bool xd = nullable == null ? new NullException("Valor invalido").ThrowNullException() : print("Valor Valido");
+                //ternary(nullable);
+            }
+            catch(NullException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            // 3.- Anomymus & 
+            var anonimo = new { Name = "Azereth", Age = 21};
+            Console.WriteLine($"Name: {anonimo.Name}\nAge: {anonimo.Age}");
+            dynamic dynamicVar = 5.2;
+            Console.WriteLine(dynamicVar);
+            dynamicVar = "I'm String";
+            Console.WriteLine(dynamicVar);
+            dynamicVar = new { Figure = "Triangle", Base = 3, height = 2.5 };
+            Console.WriteLine(dynamicVar);
+        }
+
+        private static bool print(string _string) {
+            Console.WriteLine(_string);
+            return true;
         }
     }
 }
