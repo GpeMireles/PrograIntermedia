b4861f1d0ce31d12dbfdf8daba60bd3657b1e4dd
GpeMireles <46947971+GpeMireles@users.noreply.github.com>
2022-08-30 12:10:25 -0500

    Update GenericMapExtension.cs

--- GenericMapExtension.cs
+++ GenericMapExtension.cs
@@ -7,9 +7,6 @@ using System.Threading.Tasks;
 
 namespace PrograIntermedia {
     internal static class GenericMapExtension {
-        public static void aver() {
-            Console.WriteLine();
-        }
         public static T First<T>(this GenericMap<T> map) where T : Abstract {
             return map[0];
         }
